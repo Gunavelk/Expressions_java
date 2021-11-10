aed515907190a81897bdc474cf7aa599b13d9789
Gunavelk <93586392+Gunavelk@users.noreply.github.com>
2021-11-10 16:07:59 +0530

    Add files via upload

---
+++ Comparing.java
@@ -0,0 +1,12 @@
+
+public class Comparing {
+	public static void main(String args[]) {
+		int x = 8, y = 4;
+		System.out.println("x<y:" + (x < y));
+		System.out.println("x>y:" + (x > y));
+		System.out.println("x<=y:" + (x <= y));
+		System.out.println("x>=y:" + (x >= y));
+		System.out.println("x==y:" + (x == y));
+		System.out.println("x!=y:" + (x != y));
+	}
+}
