0b4b77494ac60d76aa93bae51a5e1520f441fb16
Gunavelk <93586392+Gunavelk@users.noreply.github.com>
2021-11-10 16:11:34 +0530

    Add files via upload

---
+++ Values.java
@@ -0,0 +1,14 @@
+
+public class Values {
+	public static void main(String args[]) {
+		int i = 4;
+		i += 2;
+		System.out.println(i);
+		i -= 1;
+		System.out.println(i);
+		i *= 3;
+		System.out.println(i);
+		i /= 2;
+		System.out.println(i);
+	}
+}
